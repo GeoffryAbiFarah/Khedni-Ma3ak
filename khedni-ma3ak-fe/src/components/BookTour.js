e57ad13632aa28ec2025c60a54620dcbe48fd827
Marven-AC <marven901@gmail.com>
2020-12-24 11:44:13 +0200

    book tour part 1

--- khedni-ma3ak-fe/src/components/BookTour.js
+++ khedni-ma3ak-fe/src/components/BookTour.js
@@ -1,11 +1,43 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { useInput } from './hooks/input-hook';
+import DatePickers from "./Date";
+import ControlledOpenSelect from "./Selector";
 
 function BookTour() {
+  const { value: firstName, bind: bindFirstName, reset: resetFirstName } = useInput('');
+  const { value: lastName, bind: bindLastName, reset: resetLastName } = useInput('');
+  const { value: email, bind: bindEmail, reset: resetEmail } = useInput('');
+
+
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    alert(`Your tour has been booked, please check your email for confirmation!`);
+    resetFirstName();
+    resetLastName();
+    resetEmail();
+  }
   return (
-    <div>
-      Book Tour
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        First Name:
+        <input type="text" {...bindFirstName} />
+      </label>
+      <br />
+      <label>
+        Last Name:
+        <input type="text" {...bindLastName} />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input type="text" {...bindEmail} />
+      </label>
+      <br />
+      <DatePickers />
+      <ControlledOpenSelect />
+      <input type="submit" value="Submit" />
+    </form>
   );
 }
 
